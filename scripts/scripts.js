85ab56b48729469ac1c71f2fa8d2b3f381cffa76
marcosocon <oconmarcos97@gmail.com>
2017-04-09 15:31:45 -0300

    Update 2017-04-09T18:31:41.716Z

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+$(function() {
+  return $('#menu').on("click", function() {
+    return $('.container').toggleClass('hide-drawer');
+  });
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6InNjcmlwdHMuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKGZ1bmN0aW9uKCkge1xuICByZXR1cm4gJCgnI21lbnUnKS5vbihcImNsaWNrXCIsIGZ1bmN0aW9uKCkge1xuICAgIHJldHVybiAkKCcuY29udGFpbmVyJykudG9nZ2xlQ2xhc3MoJ2hpZGUtZHJhd2VyJyk7XG4gIH0pO1xufSk7XG4iXX0=
--- src/scripts/main.coffee
+++
@@ -1,3 +0,0 @@
-$ ->
-	$('#menu').on "click", ->
-		console.log('click on menu, should close drawer')
